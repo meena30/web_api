69b54a1f0f37af1b1e26292082abdd2e0788f2d0
Meena P <meena.p@optisolbusiness.com>
2020-02-28 20:27:14 +0530

    login based on role

--- login.php
+++ login.php
@@ -17,7 +17,8 @@ $password = mysqli_real_escape_string($con, trim($request->password));
 
 $response = [
       'id' =>1,
-      'email'    => $email,
+      'email'  => $email,
+      'role' => 'Admin',
       'token' => 'eyJpc3MiOiJ0b3B0YWwuY29tIiwiZXhwIjoxNDI2NDIwODAwLCJodHRwOi8vdG9wdGFsLmNvbS9qd3RfY2xhaW1zL2lzX2FkbWluIjp0cnVlLCJjb21wYW55IjoiVG9wdGFsIiwiYXdlc29tZSI6dHJ1ZX0'
     ];
 
